d47e9f736db7bc84c633a11addd38960d4c1ef57
anju2408 <anjudewangan2000@gmail.com>
2024-07-13 13:08:35 +0530

    media changes

--- media.php
+++ media.php
@@ -30,6 +30,8 @@
   <noscript>
     <link rel="stylesheet" href="./assets/css/style.css">
   </noscript>
+  <link rel="stylesheet" href="./assets/css/content-box.css">
+  <link rel="stylesheet" href="./assets/css/image-box.css">
   <link rel="stylesheet" href="./assets/css/skin.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
   <script src="./assets/js/script.js" defer></script>
